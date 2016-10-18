c6837d93a5156ce7d7982ebba3ffcf1be28d5aa5
sigfrido <sigfrido@tiscali.it>
2016-10-18 12:52:31 +0200

    Added pedestrian view; minor changes to views

--- 03-export_osmosis_pg.sh
+++ 03-export_osmosis_pg.sh
@@ -1,11 +1,11 @@
 #! /bin/bash 
 
-
 # Create the views
-VIEWS=`./build_all_views.py | grep -v 'skip' | grep '\[' | awk '{print $1}'`
+VIEWS="aree_pedonali crossing highway locref_no_tags multipoly_ways subway subway_entrance subway_station traffic_sign tram pedestrian"
+# turn_restrictions 
 
 for VIEW in $VIEWS
 do
     echo "Exporting $VIEW..."
-    #~ ogr2ogr -t_srs epsg:3003 -f "MapInfo file" $OUTPATH/$VIEW.tab "PG:dbname='osmosis' host='$DB_HOST' port='$DB_PORT' user='$DB_USERNAME' password='$DB_PASSWORD' active_schema=$DB_SCHEMA" $VIEW
+    ogr2ogr -skipfailures -t_srs epsg:3003 -f "MapInfo file" $OUTPATH/$VIEW.tab "PG:dbname='osmosis' host='$DB_HOST' port='$DB_PORT' user='$DB_USERNAME' password='$DB_PASSWORD' active_schema=$DB_SCHEMA" $VIEW
 done;
